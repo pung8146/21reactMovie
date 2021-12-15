27dc85b6f3a8af04d4f50a52061ca9ef3be4315a
pung8146 <pung8146@naver.com>
2021-12-15 16:21:58 +0900

    css 스타일 입히는 방식중 module.css 사용하는방법

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Counter from './Counter';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
